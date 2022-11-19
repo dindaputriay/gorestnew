523a6407e8baf82cb736443d8d20d3247a5f9e92
dindaputriay <dindaputri@mhs.mdp.ac.id>
2022-11-19 09:27:49 +0700

    new

---
+++ script.js
@@ -0,0 +1,70 @@
+let listUser = document.getElementById("listUser")
+let email = document.getElementById("email")
+let name = document.getElementById("name")
+let gander = document.getElementById("gender")
+let status = document.getElementById("status")
+let alert = document.getElementById("alert")
+getUser()
+
+function getUser() {
+    fetch("https://gorest.co.in/public/v2/users")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            data.forEach(showUser)
+        })
+        .catch(error => {
+            console.log(error)
+        });
+}
+
+function showUser(value, index) {
+    listUser.innerHTML += `<tr>
+    <td>${value.email}</td>
+    <td>${value.name}</td>
+    <td>${value.gender}</td>
+    <td>${value.status}</td>
+    <td><button class="btn btn-info" onclick="editUser(${value.id})">Edit</button>
+    <button class="btn btn-danger" onclick="deleteUser(${value.id})">Delete</button></td>
+    </tr>`
+}
+
+function deleteUser(id) {
+    console.log("Hapus data id: " + id)
+    fetch("https://gorest.co.in/public/v2/users/" + id, {
+            method: "DELETE",
+            headers: {
+                Authorization: "Bearer 51530658d1529e61cbe231c3fd226b95cc69da691cad7075f0ead6408f656508"
+            }
+        })
+        .then(response => {
+            console.log(response)
+            listUser.innerHTML = "" // kosongkan tabel list user
+            getUser() // panggil function getUser()
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
+
+function createUser() {
+    console.log("Button Simpan Ditekan")
+    fetch("https://gorest.co.in/public/v2/users", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': "Bearer 51530658d1529e61cbe231c3fd226b95cc69da691cad7075f0ead6408f656508"
+            },
+            body: JSON.stringify({
+                "email": email.value,
+                "name": name.value,
+                "gender": gender.value,
+                "status": status.value
+            })
+        })
+        .then(response => response.json())
+        .then(result => console.log(result))
+        .catch(error => {
+            console.log(error)
+        })
+}
