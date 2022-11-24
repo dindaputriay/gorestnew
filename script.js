1e82b4363d00ea58ed5f0d509eacdd68856aa8fa
dindaputriay <dindaputri@mhs.mdp.ac.id>
2022-11-24 09:30:07 +0700

    2

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let name = document.getElementById("name")
 let gander = document.getElementById("gender")
 let status = document.getElementById("status")
 let alert = document.getElementById("alert")
+let btnCreate = document.getElementById("btnCreate")
 getUser()
 
 function getUser() {
@@ -47,13 +48,14 @@ function deleteUser(id) {
         })
 }
 
-function createUser() {
-    console.log("Button Simpan Ditekan")
-    fetch("https://gorest.co.in/public/v2/users", {
+function createUser(statusSimpan = 0) {
+    if (statusSimpan == 0) {
+        //simpan data
+        console.log("Button simpan ditekan")
+        fetch("https://gorest.co.in/public/v2/users", {
             method: "POST",
             headers: {
-                'Content-Type': 'application/json',
-                'Authorization': "Bearer 51530658d1529e61cbe231c3fd226b95cc69da691cad7075f0ead6408f656508"
+                Authorization: "Bearer 51530658d1529e61cbe231c3fd226b95cc69da691cad7075f0ead6408f656508"
             },
             body: JSON.stringify({
                 "email": email.value,
@@ -62,9 +64,49 @@ function createUser() {
                 "status": status.value
             })
         })
-        .then(response => response.json())
-        .then(result => console.log(result))
-        .catch(error => {
-            console.log(error)
-        })
+
+            .then(response => {
+                response.json()
+                console.log(response.status)
+            })
+            .then(result)
+        
+            if (response.status == 201) { //user berhasil disimpan siserver Gorest
+                alert.innerHTML = '<div class = "alert alert-success" > User berhasil disimpan < /div>'
+            } else ( // user gagal disimpan keserver gorest
+                alert.innerHTML = '<div class = "alert alert-danger" > User gagal disimpan < /div>'
+            )
+
+                .then(result => {
+                    console.log(result)
+                })
+        
+
+    } else {
+        //  ubah data
+        console.log("Button simpan ditekan")
+    }
+
 }
+    
+     function editUser(id) {
+         console.log(id)
+         fetch("https://gorest.co.in/public/v2/users/" + id)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+               //set value input email, name, gender, dan status
+
+                email.value = data.email
+                name.value = data.name
+                gender.value = data.gender
+                status.value = data.status
+                 //ubah teks "simpan" menjadi "ubah"
+                btnCreate.innerHTML = "ubah3"
+                //onClick="createUser(1)"
+                btnCreate.setAttribute("onclick", "createUser(1)")    
+            })
+            .catch(error => {
+                console.log(error)
+            });
+    }
